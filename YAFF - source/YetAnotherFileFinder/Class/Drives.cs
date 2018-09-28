bae74489d76a682a3672dde8b952dc87d284cd35
Senistan <senistan.jegarajasingam@cpnv.ch>
2018-09-28 10:19:24 +0200

    Added Classes

---
+++ YAFF - source/YetAnotherFileFinder/Class/Drives.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * CHANGER
+ * LES
+ * TYPES VOIDS
+ * MERCI
+ */
+
+namespace YetAnotherFileFinder.Class
+{
+    class Drives
+    {
+        public void LoadDrive(){
+
+        }
+
+        public void ShowDrive(){
+
+        }
+    }
+}
---
+++ YAFF - source/YetAnotherFileFinder/Class/Files.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * CHANGER
+ * LES
+ * TYPES VOIDS
+ * MERCI
+ */
+
+
+namespace YetAnotherFileFinder.Class
+{
+    class Files
+    {
+        public void GetFiles(){
+
+        }
+
+        public void ShowResults(){
+        }
+
+        public void ConvertFile(){
+        }
+
+        public void ReadFile(){
+        }
+    }
+}
