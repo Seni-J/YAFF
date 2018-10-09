a28b00f073c68c0ac8e7c84e08cbb2c4b54bcb4d
JarodS <strecky1800@gmail.com>
2018-10-09 08:58:36 +0200

    Auto stash before rebase of "master"
    Added Folder loading class with tree view

---
+++ YAFF - source/YetAnotherFileFinder/Folder.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace YetAnotherFileFinder
+{
+    partial class frmFolder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvFolder = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tvFolder
+            // 
+            this.tvFolder.Location = new System.Drawing.Point(3, 1);
+            this.tvFolder.Name = "tvFolder";
+            this.tvFolder.Size = new System.Drawing.Size(279, 449);
+            this.tvFolder.TabIndex = 23;
+            // 
+            // frmFolder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 450);
+            this.Controls.Add(this.tvFolder);
+            this.Name = "frmFolder";
+            this.Text = "Folders";
+            this.Load += new System.EventHandler(this.Folder_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvFolder;
+    }
+}
---
+++ YAFF - source/YetAnotherFileFinder/Folder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace YetAnotherFileFinder
+{
+    public partial class frmFolder : Form
+    {
+        public frmFolder()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Folder_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public void LoadFolder(string selectedDrive)
+        {
+            ListDirectory(tvFolder, selectedDrive);
+        }
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            
+            return directoryNode;
+        }
+    }
+}
